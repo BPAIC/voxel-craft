82398ec267202d44b2969310bd47f95ee85264a6
B1gergame <37130161+B1gergame@users.noreply.github.com>
2025-06-11 20:27:28 +0300

    Fix indentation in chunk_manager

--- scripts/chunk_manager.gd
+++ scripts/chunk_manager.gd
@@ -22,7 +22,7 @@ func _process(_delta: float) -> void:
 func get_current_player_chunk_position() -> Vector2i:
 	# Warning: need to find way how to syncronize size of chunk
 	var pcp = floor(player.position / 8)
-	return Vector2i(pcp.x, pcp.z) 
+	return Vector2i(pcp.x, pcp.z)
 
 func load_chunks_at_player() -> void:
 	for x in range(-radius, radius + 1):
@@ -30,37 +30,37 @@ func load_chunks_at_player() -> void:
 			var chunk_position = player_chunk_position + Vector2i(x, z)
 			if loaded_chunks.has(chunk_position):
 				continue
-                        var chunk = Chunk.new()
-                        chunk.chunk_position = chunk_position
-                        loaded_chunks[chunk_position] = chunk
-                        chunk.generated.connect(_on_chunk_generated)
-                        chunk.start_generation()
+			var chunk = Chunk.new()
+			chunk.chunk_position = chunk_position
+			loaded_chunks[chunk_position] = chunk
+			chunk.generated.connect(_on_chunk_generated)
+			chunk.start_generation()
 
 func unload_chunks() -> void:
-        var loaded_chunks_positions: Array = loaded_chunks.keys()
-        for chunk_position in loaded_chunks_positions:
-                if (chunk_position.x - radius > player_chunk_position.x or \
-                        chunk_position.x + radius < player_chunk_position.x or \
-                        chunk_position.y - radius > player_chunk_position.y or \
-                        chunk_position.y + radius < player_chunk_position.y) and \
-                        loaded_chunks.has(chunk_position):
-                        var chunk: Chunk = loaded_chunks[chunk_position]
-                        if chunk.get_parent():
-                                remove_child(chunk)
-                        chunk.queue_free()
-                        loaded_chunks.erase(chunk_position)
+	var loaded_chunks_positions: Array = loaded_chunks.keys()
+	for chunk_position in loaded_chunks_positions:
+		if (chunk_position.x - radius > player_chunk_position.x or \
+			chunk_position.x + radius < player_chunk_position.x or \
+			chunk_position.y - radius > player_chunk_position.y or \
+			chunk_position.y + radius < player_chunk_position.y) and \
+			loaded_chunks.has(chunk_position):
+			var chunk: Chunk = loaded_chunks[chunk_position]
+			if chunk.get_parent():
+				remove_child(chunk)
+			chunk.queue_free()
+			loaded_chunks.erase(chunk_position)
 
 func _on_chunk_generated(chunk: Chunk) -> void:
-        if not loaded_chunks.has(chunk.chunk_position):
-                chunk.queue_free()
-                return
-        if chunk.chunk_position.x - radius > player_chunk_position.x or \
-                        chunk.chunk_position.x + radius < player_chunk_position.x or \
-                        chunk.chunk_position.y - radius > player_chunk_position.y or \
-                        chunk.chunk_position.y + radius < player_chunk_position.y:
-                loaded_chunks.erase(chunk.chunk_position)
-                chunk.queue_free()
-                return
+	if not loaded_chunks.has(chunk.chunk_position):
+		chunk.queue_free()
+		return
+	if chunk.chunk_position.x - radius > player_chunk_position.x or \
+			chunk.chunk_position.x + radius < player_chunk_position.x or \
+			chunk.chunk_position.y - radius > player_chunk_position.y or \
+			chunk.chunk_position.y + radius < player_chunk_position.y:
+		loaded_chunks.erase(chunk.chunk_position)
+		chunk.queue_free()
+		return
 	add_child(chunk)
 	if chunk.chunk_position == player_chunk_position:
 		var pcp = floor(player.position / 8)
