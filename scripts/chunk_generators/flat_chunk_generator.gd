94b46ffd8afdaf883a59aa2f6db50208ea81ae81
B1gergame <37130161+B1gergame@users.noreply.github.com>
2025-06-11 20:38:44 +0300

    Add coding guidelines and preload optimization

--- scripts/chunk_generators/flat_chunk_generator.gd
+++ scripts/chunk_generators/flat_chunk_generator.gd
@@ -1,7 +1,8 @@
 class_name FlatChunkGenerator extends Resource
+var _planks_res: BlockResource = preload("res://assets/blocks/planks.tres")
 
 func calc_block(block_position: Vector3i) -> BlockResource:
 	if block_position.y < 1:
-		return load("res://assets/blocks/planks.tres")
+		return _planks_res
 		#var block = BlockResource.new()
 	return null
