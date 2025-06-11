812ac2f217df3612106744b0f54c3a55d18abb8c
Bpaic <mega98byte@gmail.com>
2025-06-11 21:10:40 +0300

    test

--- scripts/chunk_generators/flat_chunk_generator.gd
+++ scripts/chunk_generators/flat_chunk_generator.gd
@@ -1,8 +1,7 @@
 class_name FlatChunkGenerator extends Resource
-var _planks_res: BlockResource = preload("res://assets/blocks/planks.tres")
 
 func calc_block(block_position: Vector3i) -> BlockResource:
 	if block_position.y < 1:
-		return _planks_res
+		return load("res://assets/blocks/planks.tres")
 		#var block = BlockResource.new()
 	return null
